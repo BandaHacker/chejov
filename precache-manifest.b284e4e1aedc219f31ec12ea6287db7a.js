16a6000e09cf7b48f907fa2efd2068af8da642ee
mcarvajal <esintomatico@gmail.com>
2019-01-03 10:51:29 +0100

    Updates

---
+++ precache-manifest.b284e4e1aedc219f31ec12ea6287db7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "144a8c63f35e6a0e4d9d",
+    "url": "/chejov/static/css/main.a63ee6ba.chunk.css"
+  },
+  {
+    "revision": "144a8c63f35e6a0e4d9d",
+    "url": "/chejov/static/js/main.144a8c63.chunk.js"
+  },
+  {
+    "revision": "3f407593819c91a6639f",
+    "url": "/chejov/static/css/1.d63d4f85.chunk.css"
+  },
+  {
+    "revision": "3f407593819c91a6639f",
+    "url": "/chejov/static/js/1.3f407593.chunk.js"
+  },
+  {
+    "revision": "c9aaae2687756c87f734",
+    "url": "/chejov/static/js/runtime~main.c9aaae26.js"
+  },
+  {
+    "revision": "13bba341f84d537d5f0de12b7e90ea5e",
+    "url": "/chejov/static/media/logo.13bba341.png"
+  },
+  {
+    "revision": "f60879f138f82d0f423679a0060fb2a9",
+    "url": "/chejov/index.html"
+  }
+];
